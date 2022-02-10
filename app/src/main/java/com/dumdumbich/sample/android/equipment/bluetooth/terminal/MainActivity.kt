56d5766647e20fe8f733df6c78d812a8b1b3e622
DumDumbIch <dumdumbich@gmail.com>
2022-02-10 14:20:46 +0300

    Add view binding

--- app/src/main/java/com/dumdumbich/sample/android/equipment/bluetooth/terminal/MainActivity.kt
+++ app/src/main/java/com/dumdumbich/sample/android/equipment/bluetooth/terminal/MainActivity.kt
@@ -2,10 +2,15 @@ package com.dumdumbich.sample.android.equipment.bluetooth.terminal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dumdumbich.sample.android.equipment.bluetooth.terminal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ui: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
     }
+
 }
