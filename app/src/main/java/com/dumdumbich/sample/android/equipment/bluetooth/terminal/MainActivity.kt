632be29beb29b3c621db05808b92b54e61d88e99
DumDumbIch <dumdumbich@gmail.com>
2022-02-10 14:12:33 +0300

    Initial commit

---
+++ app/src/main/java/com/dumdumbich/sample/android/equipment/bluetooth/terminal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dumdumbich.sample.android.equipment.bluetooth.terminal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
