627e7ccb7a5451da71a9b8baec30ea4dff8a32ef
DumDumbIch <dumdumbich@gmail.com>
2022-02-10 15:18:39 +0300

    Check enabling and turn on bluetooth

--- app/src/main/java/com/dumdumbich/sample/android/equipment/bluetooth/terminal/App.kt
+++ app/src/main/java/com/dumdumbich/sample/android/equipment/bluetooth/terminal/App.kt
@@ -1,12 +1,17 @@
 package com.dumdumbich.sample.android.equipment.bluetooth.terminal
 
 import android.app.Application
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.content.pm.PackageManager
 
 
 class App : Application() {
 
-    private fun PackageManager.missingSystemFeature(name: String): Boolean = !hasSystemFeature(name)
+    private lateinit var bluetoothManager: BluetoothManager
+    lateinit var bluetoothAdapter: BluetoothAdapter
+
 
     override fun onCreate() {
         super.onCreate()
@@ -14,6 +19,14 @@ class App : Application() {
         packageManager.takeIf { it.missingSystemFeature(PackageManager.FEATURE_BLUETOOTH) }?.also {
             throw IllegalAccessException("Bluetooth not supported on this device")
         }
+        bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothAdapter = bluetoothManager.adapter
     }
 
+    private fun PackageManager.missingSystemFeature(name: String): Boolean = !hasSystemFeature(name)
+
 }
+
+
+val Context.app: App
+    get() = applicationContext as App
--- app/src/main/java/com/dumdumbich/sample/android/equipment/bluetooth/terminal/MainActivity.kt
+++ app/src/main/java/com/dumdumbich/sample/android/equipment/bluetooth/terminal/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.dumdumbich.sample.android.equipment.bluetooth.terminal
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.dumdumbich.sample.android.equipment.bluetooth.terminal.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var ui: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ui = ActivityMainBinding.inflate(layoutInflater)
         setContentView(ui.root)
+
+        app.bluetoothAdapter.takeIf { !it.isEnabled }.also {
+            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            val bluetoothEnableLauncher: ActivityResultLauncher<Intent> =
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                    if (result.resultCode == Activity.RESULT_OK) {
+                        Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            bluetoothEnableLauncher.launch(enableIntent)
+        }
+
     }
 
 }
