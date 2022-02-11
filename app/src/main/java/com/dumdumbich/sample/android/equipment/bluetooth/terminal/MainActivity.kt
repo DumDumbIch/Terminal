f10fada57e24aa3c82492ea3900d2b12905f7053
DumDumbIch <dumdumbich@gmail.com>
2022-02-11 20:50:48 +0300

    Bluetooth turn on-off

--- app/src/main/java/com/dumdumbich/sample/android/equipment/bluetooth/terminal/MainActivity.kt
+++ app/src/main/java/com/dumdumbich/sample/android/equipment/bluetooth/terminal/MainActivity.kt
@@ -1,48 +1,72 @@
 package com.dumdumbich.sample.android.equipment.bluetooth.terminal
 
-import android.annotation.SuppressLint
+import android.Manifest
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.dumdumbich.sample.android.equipment.bluetooth.terminal.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "@@@ Terminal: MainActivity"
+
+    companion object {
+        private val TAG = "@@@ " + MainActivity::class.java.simpleName
+    }
+
     private lateinit var ui: ActivityMainBinding
 
+    private val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+    private val bluetoothEnableLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                ui.bluetoothTurnOnOffButton.text = "ON"
+            }
+        }
 
-    @SuppressLint("MissingPermission")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ui = ActivityMainBinding.inflate(layoutInflater)
         setContentView(ui.root)
 
+        ui.bluetoothStatusTextView.text = "Disconnected"
+        if (app.bluetoothAdapter.isEnabled) {
+            ui.bluetoothTurnOnOffButton.text = "ON"
+        } else {
+            ui.bluetoothTurnOnOffButton.text = "OFF"
+        }
+
+        ui.bluetoothTurnOnOffButton.setOnClickListener {
+            if (app.bluetoothAdapter.isEnabled) {
+                bluetoothOff()
+            } else {
+                bluetoothOn()
+            }
+        }
+
+    }
+
+    private fun bluetoothOn() {
         app.bluetoothAdapter.takeIf { !it.isEnabled }.also {
-            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            val bluetoothEnableLauncher: ActivityResultLauncher<Intent> =
-                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                    if (result.resultCode == Activity.RESULT_OK) {
-                        Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_SHORT).show()
-                    }
-                }
             bluetoothEnableLauncher.launch(enableIntent)
         }
+    }
 
-        val pairedDevices: Set<BluetoothDevice>? = app.bluetoothAdapter.bondedDevices
-        pairedDevices?.forEach { device ->
-            val deviceName = device.name
-            val deviceHardwareAddress = device.address // MAC address
-            Log.d(TAG, "onCreate() called with: device = [ $deviceName : $deviceHardwareAddress ] ")
+    private fun bluetoothOff() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            app.bluetoothAdapter.disable()
+            ui.bluetoothTurnOnOffButton.text = "OFF"
         }
-
     }
 
 }
